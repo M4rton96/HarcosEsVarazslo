8a1334fbbd1722964591d6aa30b401cf8e42ef77
Márton Turi <lamax696@gmail.com>
2023-05-16 20:55:10 +0200

    test commit

--- src/harcosesvarazslo/HarcosEsVarazslo.java
+++ src/harcosesvarazslo/HarcosEsVarazslo.java
@@ -14,7 +14,7 @@ public class HarcosEsVarazslo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int i = 1;
     }
     
 }
