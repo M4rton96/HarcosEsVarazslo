20b8716d49cf52e4dd63afc44c10d5affea98556
Márton Turi <lamax696@gmail.com>
2023-05-17 19:12:18 +0200

    alapfeladat teljes

--- src/harcosesvarazslo/HarcosEsVarazslo.java
+++ src/harcosesvarazslo/HarcosEsVarazslo.java
@@ -4,17 +4,46 @@
  */
 package harcosesvarazslo;
 
-/**
- *
- * @author Petyus Sándor
- */
+import java.util.Random;
+
 public class HarcosEsVarazslo {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        int i = 1;
+    static Random random = new Random();
+
+    public static int d6() {
+        return random.nextInt(6) + 1;
     }
-    
+
+    public static void main(String[] args) {
+        int harcosEletero = d6() + 3;
+        int varazsloEletero = d6() + 3;
+
+        System.out.println("H_V --> H:" + harcosEletero + ", V:" + varazsloEletero);
+
+        while (harcosEletero > 0 && varazsloEletero > 0) {
+            int harcosLepes = random.nextInt(3);
+            int varazsloLepes = random.nextInt(3);
+
+            if (harcosLepes == varazsloLepes) {
+                StringBuilder harcAllas = new StringBuilder("___");
+                harcAllas.setCharAt(harcosLepes, 'X');
+                
+                int harcosSebzes = d6();
+                int varazsloSebzes = d6();
+                
+                harcosEletero -= varazsloSebzes;
+                varazsloEletero -= harcosSebzes;
+                
+                System.out.println(harcAllas + " harc: --> H:" + harcosEletero + ", V:" + varazsloEletero);
+            } else {
+                StringBuilder allas = new StringBuilder("___");
+                
+                allas.setCharAt(harcosLepes, 'H');
+                allas.setCharAt(varazsloLepes, 'V');
+
+                System.out.println(allas + " --> H:" + harcosEletero + ", V:" + varazsloEletero);
+            }
+        }
+    }
+
 }
